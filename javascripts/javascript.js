6b61dadcdc1cb048d706bcea622bb8790058a824
Henro Potgieter <henro47@gmail.com>
2020-08-27 09:24:07 +0200

    Anitmations Added for Skills
    Animations added for skills in css.

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,3 +1,22 @@
 function tryMe(arg){
     document.write(arg) ;
 }
+
+var i = 0;
+function move(percentage) {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= percentage) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
